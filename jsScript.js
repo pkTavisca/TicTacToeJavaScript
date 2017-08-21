35c5a30060d3d538248504d199c83f4b1e04fb30
Pratik Kumar <pkumar@tavisca.com>
2017-08-21 19:06:34 +0530

    completed barebones assignment

--- jsScript.js
+++ jsScript.js
@@ -1,26 +1,78 @@
 var p1 = {
     name: 'Player1',
-    icon: 'circle'
+    icon: 'circle',
+    playerNo: 1
 };
 var p2 = {
     name: 'Player2',
-    icon: 'cross'
+    icon: 'cross',
+    playerNo: 2
 };
 var currentPlayer = p1;
-
+setIdForAllTd();
+var currentMapArray = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+];
+var hasWon = false;
 document.getElementById("playerName").innerHTML = currentPlayer.name;
 
 document.onclick = function(event) {
+    if (hasWon) return;
     if (event.target.localName !== "td") return;
 
     var cList = event.target.classList;
     if (cList.length > 0) return;
 
-    cList.add(currentPlayer.icon);
+    setIcon(cList);
+    updateCurrentMapArray(event.target);
+
+    if (playerHasWon()) {
+        onPlayerWin();
+        return;
+    }
+
     if (currentPlayer === p1) currentPlayer = p2;
     else if (currentPlayer === p2) currentPlayer = p1;
 
     document.getElementById("playerName").innerHTML = currentPlayer.name;
+}
 
-    console.log(cList);
+function onPlayerWin() {
+    document.getElementById("currentTurn").style.display = "none";
+    document.getElementById("resultMessage").innerHTML = currentPlayer.name + " has Won!";
+    document.getElementById("resultMessage").style.display = "block";
+    document.getElementById("playAgainArticle").style.display = "block";
+    hasWon = true;
+}
+
+function playerHasWon() {
+    var n = currentPlayer.playerNo;
+    var map = currentMapArray;
+    for (var i = 0; i < map.length; i++) {
+        if (map[i][0] === map[i][1] && map[i][0] === map[i][2] && map[i][0] !== 0) return true;
+        if (map[0][i] === map[1][i] && map[0][i] === map[2][i] && map[0][i] !== 0) return true;
+    }
+    if (map[0][0] === map[1][1] && map[0][0] === map[2][2] && map[0][0] !== 0) return true;
+    if (map[0][2] === map[1][1] && map[0][2] === map[2][0] && map[0][2] !== 0) return true;
+}
+
+function setIdForAllTd() {
+    var elementArr = document.getElementsByTagName("td");
+    for (var i = 0; i < elementArr.length; i++) {
+        elementArr[i].id = i;
+    }
+}
+
+function setIcon(cList) {
+    cList.add(currentPlayer.icon);
+}
+
+function updateCurrentMapArray(target) {
+    var idNo = parseInt(target.id);
+    var row = parseInt(idNo / 3);
+    var column = idNo % 3;
+    currentMapArray[row][column] = currentPlayer.playerNo;
+    console.log(currentMapArray);
 }
