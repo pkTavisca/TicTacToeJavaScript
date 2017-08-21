1b5448ac3a11408a35933b2f3a81bd9dafe08f2c
Pratik Kumar <pkumar@tavisca.com>
2017-08-21 18:04:35 +0530

    added alternate display of circles and crosses

--- jsScript.js
+++ jsScript.js
@@ -1,1 +1,26 @@
-console.log("here");
+var p1 = {
+    name: 'Player1',
+    icon: 'circle'
+};
+var p2 = {
+    name: 'Player2',
+    icon: 'cross'
+};
+var currentPlayer = p1;
+
+document.getElementById("playerName").innerHTML = currentPlayer.name;
+
+document.onclick = function(event) {
+    if (event.target.localName !== "td") return;
+
+    var cList = event.target.classList;
+    if (cList.length > 0) return;
+
+    cList.add(currentPlayer.icon);
+    if (currentPlayer === p1) currentPlayer = p2;
+    else if (currentPlayer === p2) currentPlayer = p1;
+
+    document.getElementById("playerName").innerHTML = currentPlayer.name;
+
+    console.log(cList);
+}
